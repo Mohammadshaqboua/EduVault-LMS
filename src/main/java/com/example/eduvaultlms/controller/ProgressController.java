e6030b8b8e1304ad18e24941689c397a9afb1afb
M_Shaqboua <m.shaqboua25@gmail.com>
2026-06-19 23:32:41 +0300

    Progress Controller Done!

--- src/main/java/com/example/eduvaultlms/controller/ProgressController.java
+++ src/main/java/com/example/eduvaultlms/controller/ProgressController.java
@@ -1,4 +1,35 @@
 package com.example.eduvaultlms.controller;
 
+import com.example.eduvaultlms.dto.response.CourseProgressResponse;
+import com.example.eduvaultlms.dto.response.LessonProgressResponse;
+import com.example.eduvaultlms.service.ProgressService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/progress")
 public class ProgressController {
+    private ProgressService progressService;
+
+    @PostMapping("/lessons/{id}/complete")
+    @PreAuthorize("hasRole('STUDENT')")
+    public ResponseEntity<LessonProgressResponse> completeLesson(@PathVariable("id") UUID lessonId) {
+        return ResponseEntity.ok(progressService.completeLesson(lessonId));
+    }
+
+    @GetMapping("/courses/{id}")
+    @PreAuthorize("hasRole('STUDENT')")
+    public ResponseEntity<CourseProgressResponse> getCourseProgress(@PathVariable("id") UUID courseId) {
+        return ResponseEntity.ok(progressService.getCourseProgress(courseId));
+    }
+
+    @GetMapping("/my")
+    @PreAuthorize("hasRole('STUDENT')")
+    public ResponseEntity<List<CourseProgressResponse>> getMyProgress() {
+        return ResponseEntity.ok(progressService.getMyProgress());
+    }
 }
